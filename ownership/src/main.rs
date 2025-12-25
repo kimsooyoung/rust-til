0567eb9968d0b49f85372da696e825b56ac79902
sooyoung <sooyoung@proception.ai>
2025-12-25 00:13:23 -0800

    + owndership update / types updated

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -17,6 +17,9 @@ fn get_str_len(s: &String) -> usize {
 //     println!("string length of {} is {}", s1, s1.len());
 // }
 
+
+// This allows the main function to compile even with the warning.
+#[allow(warnings)]
 fn main() {
     // String is a reference type
     let s1 = String::from("Hello Rust!");
@@ -27,4 +30,38 @@ fn main() {
     // This does not work because s1 is already moved to _s1
     // println!("string length of {} is {}", s1, str_len);
     println!("string length of {} is {}", _s1, str_len);
+
+    // This does not work because by default, variables in Rust are immutable.
+    // When you declare `let a: i32 = 5;`, `a` cannot be changed.
+    // Therefore, trying to assign `a = 10;` will cause a compile-time error.
+    // let a: i32 = 5;
+    // a = 10;
+    // println!("a: {}", a);
+
+    let mut a: i32 = 5;
+    a = 10;
+    println!("a: {}", a);
+
+    // This does not work because const is immutable by default.
+    // const A: i32 = 5;
+    // A = 10;
+    // println!("A: {}", A);
+
+    // This works because mut is mutable by default.
+    // const mut A: i32 = 5;
+
+    const A: i32 = 5;
+    println!("A: {}", A);
+
+
+    // =========================================================================
+    // Shadowing
+    // =========================================================================
+
+    let x: i32 = 5;
+    println!("x: {}", x);
+    let x = x + 1;
+    println!("x: {}", x);
+    let x = x * 2;
+    println!("x: {}", x);
 }
